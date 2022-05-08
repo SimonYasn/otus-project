e1cf439aa9944b275788fa725f0f0be1569c1963
SimonYasn <karasekkarasek97@gmail.com>
2022-05-08 13:07:18 +0300

    some fixes

--- terraform/layer1-aws/budget.tf
+++ terraform/layer1-aws/budget.tf
@@ -1,7 +1,7 @@
 resource "aws_budgets_budget" "budget_total" {
   name              = "Month_budget"
   budget_type       = "COST"
-  limit_amount      = "120"
+  limit_amount      = "216"
   limit_unit        = "USD"
   time_period_end   = "2087-06-15_00:00"
   time_period_start = "2022-04-01_00:00"
